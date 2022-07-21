f7a674f9f0622afd0bd19ec274f6ebb244827287
ACG <acg@ACGui-MacBookPro.local>
2022-07-21 10:59:35 +0900

    Add(jungyonghoon/lecture): 에러 예외처리

--- src/lectures/repository/lectures.repository.ts
+++ src/lectures/repository/lectures.repository.ts
@@ -1,3 +1,4 @@
+import { InternalServerErrorException } from '@nestjs/common';
 import { CustomRepository } from 'src/common/decorators/custom-repository.decorator';
 import { User } from 'src/users/entity/user.entity';
 import { InsertResult, Repository } from 'typeorm';
@@ -10,30 +11,48 @@ export class LecturesRepository extends Repository<Lecture> {
     { idx }: User,
     { ...lectureInfo }: CreateLectureDto,
   ): Promise<boolean> {
-    lectureInfo['lecturerIdx'] = idx;
+    try {
+      lectureInfo['lecturerIdx'] = idx;
 
-    const { raw }: InsertResult = await this.createQueryBuilder('lectures')
-      .insert()
-      .into(Lecture)
-      .values([lectureInfo])
-      .execute();
+      const { raw }: InsertResult = await this.createQueryBuilder('lectures')
+        .insert()
+        .into(Lecture)
+        .values([lectureInfo])
+        .execute();
 
-    return !!raw.affectedRows;
+      return !!raw.affectedRows;
+    } catch (err) {
+      const error = {
+        msg: err.msg,
+        url: 'POST /lec',
+      };
+
+      throw new InternalServerErrorException(error);
+    }
   }
 
   async readAllLectures(): Promise<Lecture | Lecture[]> {
-    const lectures: Lecture | Lecture[] = await this.createQueryBuilder(
-      'lectures',
-    )
-      .select([
-        'idx AS lectureIdx',
-        'description AS lectureDescription',
-        'lecturer_idx AS lecturerIdx',
-        'DATE_FORMAT(created_at, "%Y년 %m월 %d일 %H시 %i분") AS createdAt',
-      ])
-      .orderBy('created_at', 'DESC')
-      .getRawMany();
+    try {
+      const lectures: Lecture | Lecture[] = await this.createQueryBuilder(
+        'lectures',
+      )
+        .select([
+          'idx AS lectureIdx',
+          'description AS lectureDescription',
+          'lecturer_idx AS lecturerIdx',
+          'DATE_FORMAT(created_at, "%Y년 %m월 %d일 %H시 %i분") AS createdAt',
+        ])
+        .orderBy('created_at', 'DESC')
+        .getRawMany();
 
-    return lectures;
+      return lectures;
+    } catch (err) {
+      const error = {
+        msg: err.msg,
+        url: 'GET /lec',
+      };
+
+      throw new InternalServerErrorException(error);
+    }
   }
 }
